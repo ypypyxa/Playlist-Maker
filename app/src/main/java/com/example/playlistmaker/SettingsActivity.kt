28580f1ec97df61a84fc306046df19fdfda795d7
ypypyxa <axypypy@yandex.ru>
2023-12-23 18:36:20 +0300

    Решено сразу добавить обработку обращения в техподдержку, раз в теории этому обучили

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,12 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +14,20 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<ImageButton>(R.id.back)
+        val mailToSupButton = findViewById<ImageButton>(R.id.mail_to_support)
 
         backButton.setOnClickListener {
             finish()
+        }
+
+        mailToSupButton.setOnClickListener {
+            val mailToSupIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("axypypy@yandex.ru"))
+                putExtra(Intent.EXTRA_SUBJECT, "Проблема с приложением Playlist Maker")
+                putExtra(Intent.EXTRA_TEXT, "Помогите разобраться с проблемой")
+            }
+            startActivity(mailToSupIntent)
         }
     }
 }
