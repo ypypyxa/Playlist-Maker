c2fd36f2bb4a21c1485f0ff6b791a305941d99db
ypypyxa <axypypy@yandex.ru>
2024-01-07 17:57:04 +0300

    Реализована кнопка "Поделиться приложением" Реализована кнопка "Пользовательское соглашение" Изменен текст письма и тема письма в обращении в поддержку

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -15,16 +15,27 @@ class SettingsActivity : AppCompatActivity() {
 
         val backButton = findViewById<ImageButton>(R.id.back)
         val mailToSupButton = findViewById<ImageButton>(R.id.mail_to_support)
+        val shareButton = findViewById<ImageButton>(R.id.share)
+        val agreementButton = findViewById<ImageButton>(R.id.user_agreement)
 
         backButton.setOnClickListener {
             finish()
         }
 
+        shareButton.setOnClickListener {
+            val title = "Выберите приложение"
+            val url = "https://practicum.yandex.ru/android-developer/"
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, url)
+            shareIntent.type = "aplication/octet-stream"
+            startActivity(Intent.createChooser(shareIntent, title))
+        }
+
         mailToSupButton.setOnClickListener {
             val mailToSupIntent = Intent(Intent.ACTION_SENDTO).apply {
                 val supportEmail = "axypypy@yandex.ru"
-                val messageSubject = "Проблема с приложением Playlist Maker"
-                val message = "Помогите разобраться с проблемой"
+                val messageSubject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+                val message = "Спасибо за крутое приложение!"
 
                 data = Uri.parse("mailto:")
                 putExtra(Intent.EXTRA_EMAIL, arrayOf(supportEmail))
@@ -32,6 +43,13 @@ class SettingsActivity : AppCompatActivity() {
                 putExtra(Intent.EXTRA_TEXT, message)
             }
             startActivity(mailToSupIntent)
+        }
+
+        agreementButton.setOnClickListener {
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val agreementIntent = Intent(Intent.ACTION_VIEW, url)
+
+            startActivity(agreementIntent)
         }
     }
 }
