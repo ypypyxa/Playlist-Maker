026353b1dbca1688777a33b56929dc6ae15f5f1e
ypypyxa <axypypy@yandex.ru>
2024-01-18 22:19:29 +0300

    Исправлены мелкие недочеты

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
-import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.LinearLayout
 
 class SearchActivity : AppCompatActivity() {
+    private var textValue: String = TEXT
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val backButton = findViewById<ImageButton>(R.id.back)
-        val editText = findViewById<EditText>(R.id.editText)
         val clearButton = findViewById<ImageView>(R.id.clearButton)
+        val editText = findViewById<EditText>(R.id.editText)
+
+        if (savedInstanceState != null) {
+            textValue = savedInstanceState.getString(EDITED_TEXT, TEXT)
+        }
+        editText.setText(textValue)
 
         backButton.setOnClickListener {
             finish()
@@ -26,6 +32,7 @@ class SearchActivity : AppCompatActivity() {
 
         clearButton.setOnClickListener {
             editText.setText("")
+            hideKeyboard()
         }
 
         val simpleTextWatcher = object : TextWatcher {
@@ -35,10 +42,11 @@ class SearchActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = clearButtonVisibility(s)
+                textValue = s.toString()
             }
 
             override fun afterTextChanged(s: Editable?) {
-                // empty
+                //empty
             }
         }
         editText.addTextChangedListener(simpleTextWatcher)
@@ -50,5 +58,26 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    private fun hideKeyboard() {
+        val view = currentFocus ?: return
+        val method = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        method.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(EDITED_TEXT, textValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Вторым параметром мы передаём значение по умолчанию
+        textValue = savedInstanceState.getString(EDITED_TEXT, TEXT)
+
+    }
+    companion object {
+        const val EDITED_TEXT = "EDITED_TEXT"
+        const val TEXT = ""
     }
 }
