e68e0e13b4fefd6532273097ef37637862c27254
ypypyxa <axypypy@yandex.ru>
2024-04-27 19:34:22 +0300

    Улучшаем PlaylistMaker
    activity_player
    Изменен id кнопки "Назад" под общий вид id элементов
    
    activity_search
    Добавлен ProgressBar
    
    drawable
    Добавлена иконка кнопки "Пауза"
    
    PlayerActivity
    
    Реализована логика проигрывания трека. Для этого добавлены:
    1) Переменные и константы:
    1.1) track (не столько добавлена, сколько вынесен в глобальные переменные)
    1.2) btnPlayPause (для запуска и паузы песни)
    1.3) tvPlayTime (для отображения времени воспроизведения)
    1.4) handler (для задержки обновления времени воспроизведения)
    1.5) DELAY (для того что бы задать время задержки обновления времени воспроизведения)
    1.6) DEFAULT_TIME (для сброса значения времени воспроизведения)
    1.7) STATE_DEFAULT
    1.8) STATE_PREPARED
    1.9) STATE_PLAYING
    1.10) STATE_PAUSED (для отслеживания состояния плеера)
    2) Функции:
    2.1) playbackControl() (т.к. плеер управляется одной кнопкой требуется отслеживать его состояние)
    2.2) preparePlayer() (задаем настройки плеера)
    2.3) startPlayer()
    2.4) pausePlayer()
    А так же были переопределены функции
    1) onPause
    2) onDestroy
    
    SearchActivity
    
    Для избежания двойного открытия плеера добавлена задержка ожидания нажатий в одну секунду. Для этого добавлены:
    1) Переменные:
    1.1) handler (для задержки)
    1.2) isClickAllowed (для отслеживания нажатия на элемент списка)
    1.3) searchRunnable
    1.4) progressBar
    2) Константы:
    2.1) CLICK_DEBOUNCE_DELAY
    2.2) SEARCH_DEBOUNCE_DELAY
    3) Функции:
    3.1) clickDebounce() (Для задержки нажатия на элемент)
    3.2) searchDebounce() (Для задержки поискового запроса)
    А так же изменена лямбда инициализации адаптера, собственно добавлена задержка нажатия на элемент
    Добавлена логика для того, что бы после каждой введенной буквы приложение не торопилось сразу искать введенный запрос добавлена задержка поиска треков.
    
    Strings
    Исправлена синтаксическая ошибка
    
    Track
    Добавлена переменная с адресом файла песни

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -3,13 +3,14 @@ package com.example.playlistmaker
 import java.io.Serializable
 
 data class Track(
-    val trackId: String, //Идентификатор трека
+    val trackId: String, // Идентификатор трека
     val trackName: String, // Название трека
     val artistName: String, // Имя исполнителя
     val trackTimeMillis: String, // Продолжительность трека
     val artworkUrl100: String, // Ссылка на изображение обложки
     val collectionName: String, // Название альбома
-    val releaseDate: String, //Год релиза
-    val primaryGenreName: String, //Жанр
-    val country: String //Страна исполнителя
+    val releaseDate: String, // Год релиза
+    val primaryGenreName: String, // Жанр
+    val country: String, // Страна исполнителя
+    val previewUrl: String // Адрес файла трека
     ) : Serializable
