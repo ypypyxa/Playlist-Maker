b14013eaf69f9f2dbfc955729bba5dcef15362c5
ypypyxa <axypypy@yandex.ru>
2024-09-29 21:36:12 +0300

    Домашнее задание 21го спринта: Room
    Забыл добавить файлы в прошлы коммит))

--- app/src/main/java/com/example/playlistmaker/search/data/dto/TrackDto.kt
+++ app/src/main/java/com/example/playlistmaker/search/data/dto/TrackDto.kt
@@ -1,14 +1,16 @@
 package com.example.playlistmaker.search.data.dto
 
 data class TrackDto(
-    val trackId: String, // Идентификатор трека
-    val trackName: String, // Название трека
-    val artistName: String, // Имя исполнителя
-    val trackTimeMillis: String, // Продолжительность трека
-    val artworkUrl100: String, // Ссылка на изображение обложки
-    val collectionName: String, // Название альбома
-    val releaseDate: String, // Год релиза
-    val primaryGenreName: String, // Жанр
-    val country: String, // Страна исполнителя
-    val previewUrl: String // Адрес файла трека
+    val trackId: Long,              // Идентификатор трека
+    val trackName: String,          // Название трека
+    val artistName: String,         // Имя исполнителя
+    val trackTimeMillis: String,    // Продолжительность трека
+    val artworkUrl100: String,      // Ссылка на изображение обложки
+    val collectionName: String,     // Название альбома
+    val releaseDate: String,        // Год релиза
+    val primaryGenreName: String,   // Жанр
+    val country: String,            // Страна исполнителя
+    val previewUrl: String,         // Адрес файла трека
+    var inFavorite: Boolean,        // Добавлен ли трек в избранное
+    var addToFavoritesDate: Long    // Дата добавления трека в избранное
 )
